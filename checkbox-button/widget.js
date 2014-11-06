e1c3b9e25b9d4d0a55db6e40515d0bf206593cd4
Javier Prieto Diaz <javier.diaz@ef.com>
2014-11-06 11:42:52 +0000

    secondary solution for fix ie8 button clicks

--- checkbox-button/widget.js
+++ checkbox-button/widget.js
@@ -1,21 +1,8 @@
 define([ "troopjs-dom/component/widget" ], function (Widget) {
 	return Widget.extend({
 		"dom:input/change": function ($event) {
-			var $target = $($event.target);
-			var active = $target.is(":checked");
-
-			$target
-				.closest("label")
-				.next("button")
-				.toggleClass("active", active);
-		},
-
-		"dom:button/click": function ($event) {
-			$($event.target)
-				.prev("label")
-				.click();
-
-			return false;
+			var $target = $($event.target); 
+			$target.toggleClass("checked", $target.is(":checked"));
 		}
 	});
 });
